8b148f81d5f4aac24eb961f9b08528fb73673af5
a.a.laktionov <a.a.laktionov@we-on.com>
2023-02-27 17:21:37 +0300

    fix main style

--- src/blocks/main-block/main-block.ts
+++ src/blocks/main-block/main-block.ts
@@ -1,6 +1,5 @@
 import Block from "../../utils/Block";
 import template from "./main-block.hbs";
-import styles from './main-block.module.scss';
 
 type MainBlockProps = {
 	events?: Record<string, (e?: Event) => void>;
@@ -9,7 +8,7 @@ type MainBlockProps = {
 export class MainBlock extends Block<MainBlockProps> {
 	constructor(props: MainBlockProps) {
 		super('ul', props);
-		this.element!.classList.add(styles.menu);
+		this.element!.classList.add('menu');
 	}
 
 	render() {
